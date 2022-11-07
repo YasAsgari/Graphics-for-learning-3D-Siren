8ac7e87fc06e1a33d073d98bb9bb7eaa82cf5760
Yasaman Asgari <50579308+yasamanasgarii@users.noreply.github.com>
2022-11-07 14:34:00 +0100

    Update Preprocessing.py

--- Preprocessing/Preprocessing.py
+++ Preprocessing/Preprocessing.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import trimesh
 for string in ['cow','airplane','sofa']:
     mesh = trimesh.load(f'{string}.obj')
     mesh.show()
     mesh.export(f'{string}-mesh.stl')
-
